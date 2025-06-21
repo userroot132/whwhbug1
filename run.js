dfdee047468cfba1e7a439dcfba3d91dbabff29d
userroot <sisuryaroot@gmail.com>
2025-06-21 18:11:58 +0800

    Update run.js

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-console.log('Welcome to your server!\nType your command and send it to your server on "type a command..."')
+console.log('selamat datang di egg bug wa by surya"')
 require('child_process').spawn('bash', [], {
   stdio: ['inherit', 'inherit', 'inherit', 'ipc']
 })
